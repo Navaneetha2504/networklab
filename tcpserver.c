5bc982dcd986d402b2befc75fd5ba282f318dc4b
M S Navaneetha <150831392+Navaneetha2504@users.noreply.github.com>
2025-06-20 11:07:21 +0530

    Update tcpserver.c

--- tcpserver.c
+++ tcpserver.c
@@ -31,21 +31,16 @@ void chat(int connfd){
 
 int main(){
     SAI server,client;
-    // socket create
     int sockfd, connfd, len;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     printf("Socket successfully created.....\n");
-    // server init
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(PORT);
-    //binding
     if ((bind(sockfd, (SA*)&server, sizeof(server))) == 0) 
         printf("Socket successfully binded..\n");
-    //listening
     if ((listen(sockfd, 5)) == 0) 
         printf("Server listening..\n");
-    //accept request
     len = sizeof(client);
     connfd = accept(sockfd, (SA*)&client, (socklen_t*)&len);
     printf("Server accept the client...\n");
